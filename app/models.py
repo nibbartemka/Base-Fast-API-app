ea3d7a9cfb6b7daf072b3280c92eafc09779110f
Artem Shuliatev <slenderartem2003@gmail.com>
2026-02-20 20:16:03 +1000

    FEAT: Описаны основные модели предметной области

---
+++ app/models.py
@@ -0,0 +1,62 @@
+from datetime import datetime, date
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, ForeignKey, DateTime, UniqueConstraint
+from sqlalchemy.sql import func
+
+from core import Base
+
+
+class Department(Base):
+    __tablename__ = "departments"
+    __table_args__ = (
+        UniqueConstraint("parent_id", "name", name="uq_department_parent_name"),
+    )
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(200), nullable=False)
+
+    parent_id: Mapped[int | None] = mapped_column(
+        ForeignKey("departments.id", ondelete="CASCADE"),
+        nullable=True
+    )
+
+    parent: Mapped["Department | None"] = relationship(
+        "Department",
+        remote_side=[id],
+        back_populates="sub_deps",
+    )
+
+    sub_deps: Mapped[list["Department"]] = relationship(
+        "Department",
+        back_populates="parent",
+        cascade="all, delete-orphan",
+        single_parent=True,
+    )
+
+    employees = relationship(
+        "Employee",
+        back_populates="department",
+        cascade="all, delete-orphan",
+    )
+
+
+class Employee(Base):
+    __tablename__ = 'staff'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    position: Mapped[str] = mapped_column(nullable=False)
+    hired_at: Mapped[date | None] = mapped_column(nullable=True)
+    created_ad: Mapped[datetime] = mapped_column(DateTime(timezone=True),
+                                                 server_default=func.now())
+
+    department_id: Mapped[int] = mapped_column(
+        ForeignKey("departments.id", ondelete="CASCADE"),
+        nullable=False
+    )
+
+    department = relationship(
+        "Department",
+        back_populates="employees"
+    )
