be9d80c010a1f3e2ebdb1627a528b8ccf8ef8a16
Artem Shuliatev <slenderartem2003@gmail.com>
2026-02-26 21:28:28 +1000

    FEAT: Запуск приложения

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from fastapi-backend!")
+from fastapi import FastAPI
+import uvicorn
 
+from app import create_app
+
+
+app: FastAPI = create_app()
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True
+    )
