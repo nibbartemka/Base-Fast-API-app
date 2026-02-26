6666e63ed5a25eb951f951a251fc233780bc5283
Artem Shuliatev <slenderartem2003@gmail.com>
2026-02-26 21:24:40 +1000

    FEAT: Определен функционал для запуска сервиса

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
+from .api import api_router
+from .core import init_db
+from .models import Department, Employee
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+    yield
+
+
+def create_app() -> FastAPI:
+    app: FastAPI = FastAPI(lifespan=lifespan)
+    app.include_router(api_router)
+
+    return app
