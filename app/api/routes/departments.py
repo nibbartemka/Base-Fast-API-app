134c389d9d8091b6fdecc9a212e9112d68565d4e
Artem Shuliatev <slenderartem2003@gmail.com>
2026-02-26 21:23:25 +1000

    FEAT: Добавлены зависимости

---
+++ app/api/routes/departments.py
@@ -0,0 +1,150 @@
+from typing import Any
+
+from fastapi import (APIRouter,
+                     status,
+                     HTTPException,
+                     Body)
+from app.schemas import (DepartmentResponse,
+                         DepartmentCreate,
+                         DepartmentUpdate,
+                         DepartmentDetailResponse,
+                         EmployeeCreate,
+                         EmployeeResponse)
+from app.models import Department, Employee
+from app.api.deps import (ExistingDepartment,
+                          DepDetailParams,
+                          SessionDep)
+from app.api.crud import (check_cycle, get_department_by_id,
+                          get_detailed_department)
+
+
+router: APIRouter = APIRouter(prefix="/departments",
+                              tags=["departments"])
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED,
+             response_model=DepartmentResponse)
+async def create_department(
+    session: SessionDep,
+    department: DepartmentCreate = Body(
+        description="Данные для создания департамента",
+        examples=[{
+            "name": "Back-End",
+            "parent_id": 1
+        }]
+    )
+) -> DepartmentResponse:
+    if department.parent_id:
+        parent = await get_department_by_id(
+            department.parent_id,
+            session,
+        )
+
+        if parent is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Департамент-родитель с id {department.parent_id} не найден"
+            )
+
+    new_department: Department = Department(**department.model_dump())
+
+    session.add(new_department)
+    await session.commit()
+    await session.refresh(new_department)
+
+    return new_department
+
+
+@router.post("/{id}/employees",
+             status_code=status.HTTP_201_CREATED,
+             response_model=EmployeeResponse)
+async def create_employee_for_department(
+    session: SessionDep,
+    existing_department: ExistingDepartment,
+    employee: EmployeeCreate = Body(
+        description="Данные сотрудника",
+        examples=[{
+            "name": "Ivanov Ivan Ivanovich",
+            "position": "Developer of smth",
+            "hired_at": "2026-01-01"
+        }]
+    ),
+) -> EmployeeResponse:
+    employee: Employee = Employee(
+        **employee.model_dump(),
+        department_id=existing_department.id
+    )
+
+    session.add(employee)
+    await session.commit()
+    await session.refresh(employee)
+
+    return employee
+
+
+@router.get("/{id}",
+            status_code=status.HTTP_200_OK,
+            response_model=DepartmentDetailResponse)
+async def get_department(
+    session: SessionDep,
+    existing_department: ExistingDepartment,
+    detail_params: DepDetailParams
+) -> DepartmentDetailResponse:
+    result = await get_detailed_department(
+        existing_department.id,
+        session,
+        detail_params.depth,
+        detail_params.include_employees
+    )
+
+    return result
+
+
+@router.patch("/{id}",
+              status_code=status.HTTP_200_OK,
+              response_model=DepartmentResponse)
+async def update_department(
+    session: SessionDep,
+    existing_department: ExistingDepartment,
+    update_body: DepartmentUpdate = Body(
+        description="Данные для обновления департамента",
+        examples=[{
+            "name": "John Doe Dep",
+            "parent_id": 1
+        }]
+    )
+) -> DepartmentResponse:
+    if update_body.parent_id:
+        parent_department = await get_department_by_id(
+            update_body.parent_id,
+            session,
+        )
+
+        if parent_department is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Департамент-родитель с id {update_body.parent_id} не найден"
+            )
+
+        if await check_cycle(existing_department.id,
+                             update_body.parent_id, session):
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="Невозможно обновить: цикл в иерархии"
+            )
+
+    update_dict: dict[str, Any] = update_body.model_dump(exclude_unset=True)
+
+    for key, value in update_dict.items():
+        setattr(existing_department, key, value)
+
+    try:
+        await session.commit()
+        await session.refresh(existing_department)
+        return existing_department
+    except Exception as e:
+        session.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Ошибка при обновлении"
+        ) from e
